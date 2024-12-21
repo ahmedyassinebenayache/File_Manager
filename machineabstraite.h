7bd7bdc29265ef912ef7c3c3625da87bd248a670
youcef.al <Youcef122v@gmail.com>
2024-12-21 21:55:31 +0100

    insert machineabstraite

---
+++ machineabstraite.h
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define FB 5
+#define MAX 50
+#define BM 10 //Nombre de blocsMETA
+
+
+
+typedef struct BLOCTable{
+
+int  tableAllocation[MAX];
+
+}BLOCTable;
+
+
+typedef struct Meta{
+    char fname[20];
+    int blocnum;
+    int structnum;
+    int firstbloc;
+    int Globalmode;// 0 : contigue, 1 chainee
+    int internalmode;//0 : non triée, 1 // 0 : triée
+}Meta;
+
+
+typedef struct BLOCMETA{
+    Meta V[FB];
+    int nf; //nombre de ficchier de donnees
+}BLOCMETA;
+
+
+typedef struct eleve{
+    int etat; // 0:supprime , 1:existe
+    int id;
+    char nom[30];
+    char Prenom[20];
+    char sec;
+}eleve;
+
+
+
+typedef struct BLOC_co{//pour le mode contigue
+    eleve T[FB];
+    int ne;//nombre d'element
+}BLOC_co;
+
+
+
+typedef struct BLOC_ch{//pour le mode chainee
+    eleve T[FB];
+    int next;
+    int ne; //nombre d'element
+}BLOC_ch;
+
+
